3f1226cbc164527682bc3db329c45025c76fbec8
DavidSheltonSF <david.faria012@gmail.com>
2025-01-20 16:24:00 -0300

    feat(db/users.ts): Implementing User entity

---
+++ src/db/users.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import mongoC from "./mongoConnection"
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  cpf: { type: String, required: true },
+  phone: {type: String, required: true},
+  addresses: [{
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    }
+  ],
+  bankInfo: {
+    bankName: { type: String, required: true },
+    accountNumber: { type: String, required: true },
+    agencyNumber: { type: String, required: true },
+    accountType: { type: String, required: true },
+    pixKey: {type: String, required: true}
+  },
+  authentication: {
+    password: { type: String, required: true },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+})
+
+const UserModel = mongoose.model("UsersTest", UserSchema);
+
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 
+  "authentication.sessionToken": sessionToken,
+ });
+export const getUserById = (id: String) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values)
+  .save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findByIdAndDelete(id);
+export const updateUserById = (id: String, values: Record<string, any>) => UserModel.
+ findByIdAndUpdate(id, values);
+
+
+console.log(process.env.MONGO_URI)
