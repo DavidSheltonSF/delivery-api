28c1811703e30903d994d04f91da2c4052d6bb11
DavidSheltonSF <david.faria012@gmail.com>
2025-01-21 12:28:04 -0300

    feat/style: Cleanning code and testing getUserByEmail function

--- src/db/users.test.ts
+++ src/db/users.test.ts
@@ -1,21 +1,30 @@
-import { createUser, getUserByEmail } from './users'
+import { createUser, getUserByEmail, deleteAllUsers } from './users'
 import mongoConnection from './mongoConnection';
 import mongoose from 'mongoose';
+import { random } from '../helpers';
 
 describe('mongoTest', () => {
 
   beforeAll(() => {
-    mongoConnection()
+    mongoConnection();;
   })
-  //afterAll(() => mongoose.disconnect())
+  afterAll(async () => {
+    mongoose.disconnect();
+  });
+  afterEach(async () => {
+    await deleteAllUsers();
+  })
 
   test('Create new user', async () => {
+
+    const userEmail = 'carlos@bugmail.com';
   
     const user = await createUser({
-      username: 'AAAAA',
-      email: 'test@bugmail.com',
+      username: 'Carlos Miranda',
+      email: userEmail,
       cpf: '11111111111',
       phone: '88888888888',
+      role: 'costumer',
       addresses: [
         {
           street: 'Rua Teste S', 
@@ -32,16 +41,49 @@ describe('mongoTest', () => {
         pixKey: 'test@bugmail.com'
       },
       authentication: {
-          password: 'dsfsdafsfsafsdf',
-          salt: 55
+          password: 'carlos123',
+          salt: random()
       },
     })
     
-    console.log('created')
-    const userGet = await getUserByEmail('test@bugmail.com');
-    await mongoose.disconnect();
-    console.log('dsconnected');
-  
-    expect(userGet.email).toBe('test@bugmail.com');
+    const userGet = await getUserByEmail(userEmail);
+
+    expect(userGet.email).toBe(userEmail);
   }, 50000);
-})
+
+  test('Get user by email', async () => {
+    const userData = {
+      username: 'Carlos Miranda',
+      email: 'carlos@bugmail.com',
+      cpf: '11111111111',
+      phone: '88888888888',
+      role: 'costumer',
+      addresses: [
+        {
+          street: 'Rua Teste S', 
+          city: 'Belford Roxo', 
+          state: 'RJ', 
+          zipCode: '555858848548'
+        }
+      ],
+      bankInfo: {
+        bankName: 'Itaú Unibanco', 
+        accountNumber: '14314', 
+        agencyNumber: '5445', 
+        accountType: 'typeTEst',
+        pixKey: 'test@bugmail.com'
+      },
+      authentication: {
+          password: 'carlos123',
+          salt: random()
+      },
+    }
+
+    const createdUser = await createUser(userData);
+
+    const gotUser = await getUserByEmail(userData.email);
+
+    expect(createdUser.email).toBe(gotUser.email)
+  });
+
+});
