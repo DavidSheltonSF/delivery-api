d0f5b59fa92b0caef7dee977935c5bfd842fe153
DavidSheltonSF <david.faria012@gmail.com>
2025-01-25 13:57:15 -0300

    test(Users Entity): Refactoring test file

--- src/db/users.test.ts
+++ src/db/users.test.ts
@@ -3,6 +3,57 @@ import mongoConnection from './mongoConnection';
 import mongoose from 'mongoose';
 import { random } from '../helpers';
 
+const users = [
+  {
+    username: 'Carlos Miranda',
+    email: 'carlos@bugmail.com',
+    cpf: '11111111111',
+    phone: '88888888888',
+    role: 'costumer',
+    address: {
+        street: 'Rua Teste S', 
+        city: 'Belford Roxo', 
+        state: 'RJ', 
+        zipCode: '555858848548'
+    },
+    bankInfo: {
+      bankName: 'Itaú Unibanco', 
+      accountNumber: '14314', 
+      agencyNumber: '5445', 
+      accountType: 'typeTEst',
+      pixKey: 'test@bugmail.com'
+    },
+    authentication: {
+        password: 'carlos123',
+        salt: random()
+    },
+  },
+  {
+    username: 'Jeronimo Miranda',
+    email: 'jeronimo@bugmail.com',
+    cpf: '5757757',
+    phone: '75757575',
+    role: 'restaurant owner',
+    address: {
+        street: 'Rua Guará S', 
+        city: 'Belford Roxo', 
+        state: 'RJ', 
+        zipCode: '57575757'
+    },
+    bankInfo: {
+      bankName: 'Itaú Unibanco', 
+      accountNumber: '5757', 
+      agencyNumber: '227', 
+      accountType: 'typeTEst',
+      pixKey: 'jero@bugmail.com'
+    },
+    authentication: {
+        password: 'jeronimo123',
+        salt: random()
+    },
+  }
+]
+
 describe('mongoTest', () => {
 
   beforeAll(() => {
@@ -25,14 +76,12 @@ describe('mongoTest', () => {
       cpf: '11111111111',
       phone: '88888888888',
       role: 'costumer',
-      addresses: [
-        {
+      address: {
           street: 'Rua Teste S', 
           city: 'Belford Roxo', 
           state: 'RJ', 
           zipCode: '555858848548'
-        }
-      ],
+      },
       bankInfo: {
         bankName: 'Itaú Unibanco', 
         accountNumber: '14314', 
@@ -52,38 +101,10 @@ describe('mongoTest', () => {
   }, 50000);
 
   test('Get user by email', async () => {
-    const userData = {
-      username: 'Carlos Miranda',
-      email: 'carlos@bugmail.com',
-      cpf: '11111111111',
-      phone: '88888888888',
-      role: 'costumer',
-      addresses: [
-        {
-          street: 'Rua Teste S', 
-          city: 'Belford Roxo', 
-          state: 'RJ', 
-          zipCode: '555858848548'
-        }
-      ],
-      bankInfo: {
-        bankName: 'Itaú Unibanco', 
-        accountNumber: '14314', 
-        agencyNumber: '5445', 
-        accountType: 'typeTEst',
-        pixKey: 'test@bugmail.com'
-      },
-      authentication: {
-          password: 'carlos123',
-          salt: random()
-      },
-    }
 
-    const createdUser = await createUser(userData);
+    const gotUser = await getUserByEmail(users[1].email);
 
-    const gotUser = await getUserByEmail(userData.email);
-
-    expect(createdUser.email).toBe(gotUser.email)
+    expect(gotUser.email).toBe(users[1].email)
   });
 
 });
