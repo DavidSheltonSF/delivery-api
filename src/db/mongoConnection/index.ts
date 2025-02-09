87da7a4a98b46a9b58b7e1186e6be895612ad4cf
DavidSheltonSF <david.faria012@gmail.com>
2025-02-09 15:05:29 -0300

    fix(mongoConnect): Adding await statement before mongoose.connect() function and turning the default function into an assyncronous one

--- src/db/mongoConnection/index.ts
+++ src/db/mongoConnection/index.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-export default () => {
+export default async () => {
   const MONGO_URL = process.env.MONGO_URI;
   mongoose.Promise = Promise;
-  mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
   mongoose.connection.on('error', (error: Error) => console.log(error));
 }
