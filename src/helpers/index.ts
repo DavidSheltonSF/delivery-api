e5e4e10b80daa555bc7d22830beaa53d360775a9
DavidSheltonSF <david.faria012@gmail.com>
2025-02-09 13:11:43 -0300

    feat: Implementing arraysOfObjAreEqual function, to check if two arrays of object are equal

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,3 +3,31 @@ import { randomBytes } from 'crypto';
 export const random = () => {
   return randomBytes(123).toString('base64');
 }
+
+export const arraysOfObjAreEqual = (arr1: Array<any>, arr2: Array<any>): Boolean => {
+  if (arr1.length !== arr2.length){
+    console.log('Dif length')
+    return false;
+  }
+  
+  for (let i=0; i < arr1.length; i++) {
+
+    // Removing all fields that starts with '_'
+    const obj1 = Object.keys(arr1).forEach((key: any) => {
+      if (key.startsWith('_')){
+        delete arr1[key];
+      }
+    });
+    const obj2 = Object.keys(arr1).forEach((key: any) => {
+      if (key.startsWith('_')){
+        delete arr2[key];
+      }
+    });
+
+    if (JSON.stringify(obj1) != JSON.stringify(obj2)){
+      return false
+    }
+  }
+
+  return true;
+}
